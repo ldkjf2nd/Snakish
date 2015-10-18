d8fae22eb61dd8776f6431bb805ca6a2b923d17d
Tian Guo <guotq@mcmaster.ca>
2015-10-18 19:12:43 -0400

    Start Snake class

---
+++ src/Snake.java
@@ -0,0 +1,28 @@
+import java.awt.Color;
+
+
+public class Snake {
+	private node head;
+	private boolean isCrashed;
+	private int direction;
+	
+	private class node {
+		private int x,y;
+		private node tail;
+		
+		public void node(int x, int y, node tail) {
+			this.x = x;
+			this.y = y;
+			this.tail = tail;
+		}
+		public int getX() {
+			return this.x;
+		}
+		public int gety() {
+			return this.y;
+		}
+		public node getTail() {
+			return this.tail;
+		}
+	}
+}
