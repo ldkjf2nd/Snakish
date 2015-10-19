0a38c95b92fbe2495ea1e36296f577b5f392ea94
Tian Guo <guotq@mcmaster.ca>
2015-10-19 00:32:40 -0400

    add a bit more for snake

--- src/Snake.java
+++ src/Snake.java
@@ -5,6 +5,30 @@ public class Snake {
 	private boolean isCrashed;
 	private int direction;
 	
+	public Snake() {
+		head = new node();
+		head.x = 0;head.y=0;//initial position, will modify later
+		direction = 1;
+	}
+	public void move(int dir) {
+		direction = dir;
+		update();
+	}
+	public void update() {
+		node temp = null;
+		if (direction == 1) {
+		}
+		else if (direction == 2) {
+		}
+		else if (direction ==3) {
+		}
+		else {
+		}
+		checkCrash();
+	}
+	public boolean checkCrash() {
+		return true;//will check if position of head overlaps with other things.
+	}
 	private class node {
 		private int x,y;
 		private node tail;
