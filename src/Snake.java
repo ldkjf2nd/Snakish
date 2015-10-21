3f3856497c80bcba2d3f8cf639fee0ce9812a7cc
Xintong Hu <fionahxt@hotmail.com>
2015-10-20 23:55:04 -0400

    gluhluhli

--- src/Snake.java
+++ src/Snake.java
@@ -19,41 +19,41 @@ public class Snake {
 		body = new int [x][y];
 				direction = 1;
 	}
-<<<<<<< HEAD
+
 	public void move(int dir, int player) {
-=======
+
 	
 	/**
 	 * Captures the snake's movement.
 	 * @param dir
 	 */
 	public void move(int dir) {
->>>>>>> master
+
 		direction = dir;
 		update(dir, player);
 	}
-<<<<<<< HEAD
+
 	public void update(int dir, int player) {
-=======
+
 	
 	/**
 	 * Updates the position of the snake.
 	 */
 	public void update() {
 		node temp = null;
->>>>>>> master
+
 		if (direction == 1) {
 			body[x+10][y] = player;
 		}
 		else if (direction == 2) {
 			body[x][y+10] = player;
 		}
-<<<<<<< HEAD
+
 		else if (direction ==3) {
 			body[x-10][y] = player;
-=======
+
 		else if (direction == 3) {
->>>>>>> master
+
 		}
 		else {
 			body[x][y-10] = player;
@@ -69,9 +69,8 @@ public class Snake {
 	public boolean checkCrash() {
 		return true;
 	}
-<<<<<<< HEAD
+
 }
-=======
 	
 	/**
 	 * A self-implemented modified linked-list.
@@ -117,4 +116,3 @@ public class Snake {
 		}
 	}
 }
->>>>>>> master
