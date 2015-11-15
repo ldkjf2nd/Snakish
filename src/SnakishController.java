6d935c2ae26def0bbbb53632201b4f733c0c2e67
Xin Tong Hu <huxt@mcmaster.ca>
2015-11-15 16:49:51 -0500

    so done

--- src/SnakishController.java
+++ src/SnakishController.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
 /**
@@ -10,16 +11,16 @@ public class SnakishController {
 	private SnakishModel model = null;
 	private SnakishView view = null;
 	
-	//Inputs
-	private JButton startGameButton = new JButton("Start Game");
-	private JButton aboutButton = new JButton("About");
-	private JButton exitButton = new JButton("Exit");
-	private JPanel menuPanel = null;
-	private JPanel boardPanel = null; 
-	
-	//Outputs to the view
-	private JPanel textPanel = null;
-	private JPanel topPanel = null;
+//	//Inputs
+//	private JButton startGameButton = new JButton("Start Game");
+//	private JButton aboutButton = new JButton("About");
+//	private JButton exitButton = new JButton("Exit");
+//	private JPanel menuPanel = null;
+//	private JPanel boardPanel = null; 
+//	
+//	//Outputs to the view
+//	private JPanel textPanel = null;
+//	private JPanel topPanel = null;
 	
 	/**
 	 * Constructor for SnakishController.
@@ -29,9 +30,16 @@ public class SnakishController {
 	public SnakishController(SnakishModel model, SnakishView view) {
 		this.model = model;
 		this.view = view;
+		view.addMouseListener(null);
 	}
 	
 	private void startGame() {
-		
+	}
+	
+	public void actionPerformed(ActionEvent evt) {
+		Object src = evt.getSource();
+		if (src == SnakishView.btnSG) {
+			
+		}
 	}
 }
